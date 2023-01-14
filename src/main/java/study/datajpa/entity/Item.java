65833776a62b4e0ebcbf624ac039c0fb7f3ec0f1
김진영C <jin0kim326@douzone.com>
2023-01-14 18:10:40 +0900

    3+ 새로운 엔티티를 구별하는 방법

---
+++ src/main/java/study/datajpa/entity/Item.java
@@ -0,0 +1,30 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Persistable;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item extends BaseEntity implements Persistable<String> {
+    @Id
+    private String id;
+
+    public Item(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return getCreatedDate() == null;
+    }
+}
